a4dc83624d1696e3b559b5e7de96a7bc24187e66
itz-sayak <b200122@nitsikkim.ac.in>
2023-08-21 00:06:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/shamsh/projectsmit/MainActivity.java
@@ -0,0 +1,25 @@
+package com.shamsh.projectsmit;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void startProject(View v){
+        startActivity(new Intent(MainActivity.this,Main2Activity.class));
+    }
+}
+
+
